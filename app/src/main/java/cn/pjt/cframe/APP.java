65aca750fa1627c2a716baca19072989b62cd07f
pujuntao <pujuntao@zile.ai>
2019-06-08 22:21:37 +0800

    create project

---
+++ app/src/main/java/cn/pjt/cframe/APP.java
@@ -0,0 +1,6 @@
+package cn.pjt.cframe;
+
+import android.app.Application;
+
+public class APP extends Application {
+}
---
+++ app/src/main/java/cn/pjt/cframe/MainActivity.java
@@ -0,0 +1,11 @@
+package cn.pjt.cframe;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity  extends AppCompatActivity {
+
+
+
+
+}
